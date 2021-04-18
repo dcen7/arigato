00c5122c61ba97bf9e771b6bcc0213e826918354
dcen7 <mdcengiz@gmail.com>
2021-04-18 08:06:47 -0500

    added create, edit, delete functionality

---
+++ src/main/java/com/binarycod/arigato/controllers/ProductsController.java
@@ -0,0 +1,77 @@
+package com.binarycod.arigato.controllers;
+
+import com.binarycod.arigato.domain.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/products") 
+public class ProductsController {
+
+    List<Product> productList = new ArrayList<>();
+
+    @GetMapping
+    public String getProducts(Model model){
+        model.addAttribute("products", productList);
+
+        return "product_list";
+    }
+
+    @GetMapping("/new")
+    public String newProduct(Model model){
+        model.addAttribute("product", new Product());
+        return "new_product";
+    }
+
+    @PostMapping
+    public String createProduct(Product product){
+
+        productList.add(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/delete")
+    public String deleteProduct(@RequestParam Long id){
+        productList = productList
+                .stream()
+                .filter(p -> !p.getId().equals(id))
+                .collect(Collectors.toList());
+        return "redirect:/products";
+    }
+
+    @GetMapping("/edit")
+    public String editProduct(@RequestParam Long id, Model model){
+        Optional<Product> optionalProduct = productList
+                .stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        if(!optionalProduct.isPresent())
+            return "redirect:/products";
+
+        model.addAttribute("product", optionalProduct.get());
+
+        return "edit_product";
+    }
+
+    @PostMapping("/edit")
+    public String saveProduct(Product product){
+        Optional<Product> productOld = productList
+                .stream()
+                .filter(p -> p.getId().equals(product.getId()))
+                .findFirst();
+
+        if(productOld.isPresent()){
+            productList.remove(productOld.get());
+            productList.add(product);
+        }
+        return "redirect:/products";
+    }
+
+
+}
---
+++ src/main/java/com/binarycod/arigato/domain/Product.java
@@ -0,0 +1,51 @@
+package com.binarycod.arigato.domain;
+
+public class Product {
+
+    Long id;
+    String name;
+    Double price;
+    Integer size;
+
+    public Product(){}
+
+
+    public Product(Long id, String name, Double price, Integer size) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.size = size;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+}
