2abb558877b348e98144c69a724a048cfb28d8b2
dcen7 <mdcengiz@gmail.com>
2021-05-02 12:03:36 -0500

    storerepo implemented with createStore, and getStore. Store is related to Address. RowMapper and insert logic implemented respectively.

---
+++ src/main/java/com/binarycod/arigato/domain/StoreProduct.java
@@ -0,0 +1,41 @@
+package com.binarycod.arigato.domain;
+
+public class StoreProduct {
+
+    private Integer id;
+    private Store store;
+    private Product product;
+    private Integer quantity;
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
